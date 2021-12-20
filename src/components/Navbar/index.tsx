34e357e6e1a4a9c54192eb8befcc05fc8f0807c0
Flaiva Schuenquer <flavia_schuenquer@hotmail.com>
2021-12-20 15:49:58 -0300

    Alteração foto Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as MainImage } from 'assets/images/acessibilidade.svg';
+import { ReactComponent as LogImage } from 'assets/images/logo.svg';
 import './styles.css';
 import 'bootstrap/js/src/collapse.js';
 
@@ -6,45 +7,49 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
-      <div className="container-fluid">
-        {' '}
-        {/* previne quebra de linha entre logo e itens */}
-        <Link to="/" className="nav-logo-text">
-          <h4>TICSOCIAL</h4>
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#ticsocial-navbar"
-          aria-controls="ticsocial-navbar"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="ticsocial-navbar">
-          <ul className="navbar-nav offset-md-2 main-menu">
-            <li>
-              <NavLink to="/" activeClassName="active" exact>
-                HOME
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/products" activeClassName="active">
-                CRIAR / ACOMPANHAR
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/admin" activeClassName="active">
-                PLANO
-              </NavLink>
-            </li>
-          </ul>
+    <>
+      <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
+        <div className="container-fluid">
+          {' '}
+          {/* previne quebra de linha entre logo e itens */}
+          <div>
+            <Link to="/" className="nav-logo-text">
+              <LogImage />
+            </Link>
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#ticsocial-navbar"
+            aria-controls="ticsocial-navbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="ticsocial-navbar">
+            <ul className="navbar-nav offset-md-2 main-menu">
+              <li>
+                <NavLink to="/" activeClassName="active" exact>
+                  HOME
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/products" activeClassName="active">
+                  CRIAR / ACOMPANHAR
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/admin" activeClassName="active">
+                  PLANO
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
